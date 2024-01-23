81ab42641d6a0a8fb14df3abfd2ae892b8579077
R4wd0G <wpa.sys@hotmail.com>
2024-01-22 23:12:52 -0300

    .

--- obsever_user.sh
+++ obsever_user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && apt install ruby-full && gem install lolcat > /dev/null > 2
+apt update && apt install ruby-full && gem install lolcat > /dev/null > 2&1
 cat << 'EOF' | lolcat -a -s 100 -F 0.023 -S 130
  ____    ____    ____
 /\  _`\ /\  _`\ /\  _`\
