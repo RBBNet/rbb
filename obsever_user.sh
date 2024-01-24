198cf3879c3002592c9d32a7973aafa1a8f5916e
R4wd0G <wpa.sys@hotmail.com>
2024-01-24 01:37:05 -0300

    .

--- obsever_user.sh
+++ obsever_user.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-
 welcome
-
 sleep 2
 
 curl -sL https://github.com/RBBNet/start-network/releases/download/v0.4.1%2Bpermv1/start-network.tar.gz | tar xz
 cd start-network
 ./rbb-cli node create observer
 ./rbb-cli config set nodes.observer.ports+=[\"8545:8545\"]
-
 ./rbb-cli config set nodes.observer.address=[\"0.0.0.0:30303\"]
 
  
